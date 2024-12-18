1e95caa1795a3b4ebc1dd023d0ff607f7eb175cf
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2024-12-18 15:49:55 +0000

    Coin counter UI

--- Assets/Scripts/Gamification/CoinCollection.cs
+++ Assets/Scripts/Gamification/CoinCollection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinCollection : MonoBehaviour
@@ -7,12 +8,16 @@ public class CoinCollection : MonoBehaviour
 
     private int Coin = 0;
 
+    public TextMeshProUGUI coinText;
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Coin")
         {
             Coin++;
+            coinText.text = "Coins: " + Coin.ToString();
             Debug.Log(Coin);
             Destroy(other.gameObject);
         }
