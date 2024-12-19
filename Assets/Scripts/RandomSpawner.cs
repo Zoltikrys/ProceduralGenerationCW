1da5eddad165f49f5024d76e14fe86839a0bb0d8
Christopher Bridges <162894664+Zoltikrys@users.noreply.github.com>
2024-12-19 09:46:46 +0000

    Randomly spawn coins in the dungeon

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -8,14 +8,18 @@ public class RandomSpawner : MonoBehaviour
     public GameObject prefab; //this is the object to respawn
     public int spawnCount = 10; // will spawn this many times
 
+    public DungeonCreator dungeonCreator;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        dungeonCreator = GetComponent<DungeonCreator>();
+
         for (int i = 0; i < spawnCount; i++)
         {
             // Generate random positions within a certain range to spawn the game object in
-            Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(1, 10), UnityEngine.Random.Range(-10, 10));
+            Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(0,dungeonCreator.dungeonWidth), 0.2f, UnityEngine.Random.Range(0,dungeonCreator.dungeonLength)); //spawn coins within the confines of the dungeon
             Instantiate(prefab, randomPosition, Quaternion.identity);
         }
     }
@@ -24,7 +28,7 @@ public class RandomSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(1, 10), UnityEngine.Random.Range(-10, 10));
+        //Vector3 randomPosition = new Vector3(dungeonCreator.dungeonWidth, 0.2f, dungeonCreator.dungeonLength); //spawn coins within the confines of the dungeon
         //Instantiate(prefab, randomPosition, Quaternion.identity);
     }
 }
